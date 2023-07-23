122343f314b46e3658856e606ee2bb7ee4b033b0
Robin Stuedle <robin.stuedle@gmail.com>
2023-07-23 09:54:27 +0200

    adjust default keymap, create hid version

---
+++ keyboards/sofle_choc/keymaps/hid/rules.mk
@@ -0,0 +1,16 @@
+TRI_LAYER_ENABLE = yes
+
+ENCODER_ENABLE = yes
+ENCODER_MAP_ENABLE = yes
+
+OLED_ENABLE = yes
+
+# Enable per-key lighting
+RGB_MATRIX_ENABLE = yes
+
+# Enable audio keys and system control
+EXTRAKEY_ENABLE = yes
+
+# Enable link time optimization
+# Trade longer compile time for smaller firmware size
+LTO_ENABLE = yes
